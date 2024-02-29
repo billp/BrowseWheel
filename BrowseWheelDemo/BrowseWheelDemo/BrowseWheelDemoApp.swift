e0bb0b11c898bcec4d178415fcc16993f6d22688
Bill Panagiotopoulos <billp.dev@gmail.com>
2024-02-29 18:19:43 +0200

    Add demo project

---
+++ BrowseWheelDemo/BrowseWheelDemo/BrowseWheelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowseWheelDemoApp.swift
+//  BrowseWheelDemo
+//
+//  Created by Bill Panagiotopoulos on 29/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BrowseWheelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrowseWheelDemo/BrowseWheelDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrowseWheelDemo
+//
+//  Created by Bill Panagiotopoulos on 29/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
