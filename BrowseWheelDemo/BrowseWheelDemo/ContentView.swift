91d1b9cbea992c3a48a29a962633c21a3ce91ac6
Bill Panagiotopoulos <billp.dev@gmail.com>
2024-03-01 22:54:10 +0200

    Add items offset (#2)

--- BrowseWheelDemo/BrowseWheelDemo/ContentView.swift
+++ BrowseWheelDemo/BrowseWheelDemo/ContentView.swift
@@ -25,49 +25,56 @@ struct ContentView: View {
     @State private var spacing = -80.0
     @State private var padding = 50.0
     @State private var minScale = 0.6
+    @State private var itemsOffset = 0.0
     @State private var page = 0
 
     var body: some View {
-        VStack(spacing: 40) {
-            BrowseWheel(
-                items: items,
-                page: $page,
-                spacing: $spacing,
-                padding: $padding,
-                minScale: $minScale) { item in
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(item.color)
-                        Text(item.title)
+        ScrollView {
+            VStack(spacing: 40) {
+                BrowseWheel(
+                    items: items,
+                    page: $page,
+                    spacing: $spacing,
+                    padding: $padding,
+                    minScale: $minScale,
+                    itemsOffset: $itemsOffset) { item in
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(item.color)
+                            Text(item.title)
+                        }
+                        .cornerRadius(10)
+                        .shadow(radius: 2, x: 1.5, y: 1.5)
                     }
-                    .cornerRadius(10)
-                    .shadow(radius: 2, x: 1.5, y: 1.5)
+                    .frame(height: 150)
+
+                VStack {
+                    Divider()
+
+                    Text("Item: \(page+1)")
+
+                    HStack {
+                        Button("Previous") {
+                            page -= 1
+                        }
+                        Button("Next") {
+                            page += 1
+                        }
+                    }
+                    Divider()
+
+                    Text("Spacing: \(spacing)")
+                    Slider(value: $spacing, in: -200...50)
+
+                    Text("Padding: \(padding)")
+                    Slider(value: $padding, in: -100...100)
+
+                    Text("Min Scale: \(minScale)")
+                    Slider(value: $minScale, in: 0...1)
+
+                    Text("Items Offset: \(itemsOffset)")
+                    Slider(value: $itemsOffset, in: -100...100)
                 }
-                .frame(height: 150)
-
-            VStack {
-                Divider()
-
-                Text("Item: \(page+1)")
-
-                HStack {
-                    Button("Previous") {
-                        page -= 1
-                    }
-                    Button("Next") {
-                        page += 1
-                    }
-                }
-                Divider()
-
-                Text("Spacing: \(spacing)")
-                Slider(value: $spacing, in: -200...50)
-
-                Text("Padding: \(padding)")
-                Slider(value: $padding, in: -100...100)
-
-                Text("Min Scale: \(minScale)")
-                Slider(value: $minScale, in: 0...1)
             }
         }
     }
